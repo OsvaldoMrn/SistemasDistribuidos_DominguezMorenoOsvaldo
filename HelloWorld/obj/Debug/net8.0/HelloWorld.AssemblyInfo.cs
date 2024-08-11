2bcaedfa33d4abf6f8130cf8de16aed0f3415e77
Osvaldo <dominguezmorenoosvaldo@gmail.com>
2024-08-10 22:03:56 -0600

    archivos basura

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6322fa46d1d9a99913bda56edc8ac8bfd4ae90d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+209a8a9001eef89b233aa9fbfd71654b2897ff4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
