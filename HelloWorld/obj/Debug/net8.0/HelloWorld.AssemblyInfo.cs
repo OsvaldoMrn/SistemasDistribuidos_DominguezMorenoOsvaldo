c667e4e785937874c0e72c00c1885fdeea98b879
Osvaldo <dominguezmorenoosvaldo@gmail.com>
2024-08-10 21:56:20 -0600

    archivos que se modificaron, pero que ya deberían estar ignorados con el gitignore

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6322fa46d1d9a99913bda56edc8ac8bfd4ae90d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c87d2dbcbe62ecdff0e8950c6d4e583fa64e74d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
