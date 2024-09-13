7067906c28affe5f0ccb2743e0c687ba9f665187
Osvaldo <dominguezmorenoosvaldo@gmail.com>
2024-09-12 18:50:37 -0600

    Se incluye: la primera configuracion para la Rest API

---
+++ RestApi/Services/GroupService.cs
@@ -0,0 +1,24 @@
+using RestApi.Models;
+using RestApi.Repositories;
+
+namespace RestApi.Services;
+
+public class GroupService : IGroupService
+{
+    private readonly IGroupRepository _groupRepository;
+    public GroupService(IGroupRepository groupRepository){
+        _groupRepository = groupRepository;
+    }
+    public async Task<GroupUserModel> GetGroupByIdAsync(string Id, CancellationToken cancellationToken)
+    {
+        var group = await _groupRepository.GetByIdAsync(Id, cancellationToken);
+        if(group is null){
+            return null;
+        }
+        return new GroupUserModel{
+            Id = group.Id,
+            Name = group.Name,
+            CreationDate = group.CreationDate
+        };
+    }
+}
