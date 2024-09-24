7b76f204e73567c0b7631f05f18190b68f373d63
Osvaldo <dominguezmorenoosvaldo@gmail.com>
2024-09-24 12:44:02 -0600

    cache

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511d09d3f8fb82f99719301abcf92af3a5778b8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f5bb80822ec4fa0f1509cb92495ab9ff5b8284d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
